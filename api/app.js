da999adc5f60a6b35e83f4049eacb79462f937a5
Argzon <argzon01@gmail.com>
2021-07-06 10:10:09 +0200

    Added CORS

--- api/app.js
+++ api/app.js
@@ -1,40 +1,45 @@
-'use strict';
+"use strict";
 
 // load modules
-const express = require('express');
-const morgan = require('morgan');
-const usersRoute = require('./routes/users-route');
-const coursesRoute = require('./routes/courses-route');
-const { sequelize } = require('./models');
+const cors = require("cors");
+const express = require("express");
+const morgan = require("morgan");
+const usersRoute = require("./routes/users-route");
+const coursesRoute = require("./routes/courses-route");
+const { sequelize } = require("./models");
 
 // variable to enable global error logging
-const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+const enableGlobalErrorLogging =
+  process.env.ENABLE_GLOBAL_ERROR_LOGGING === "true";
 
 // create the Express app
 const app = express();
+
+// enable cors request middleware
+app.use(cors());
 
 // setup request body JSON parsing.
 app.use(express.json());
 
 // setup morgan which gives us http request logging
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Add usersRoute routes.
-app.use('/api', usersRoute);
+app.use("/api", usersRoute);
 // Add coursesRoute routes.
-app.use('/api', coursesRoute);
+app.use("/api", coursesRoute);
 
 // setup a friendly greeting for the root route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: 'Welcome to the REST API project!',
+    message: "Welcome to the REST API project!",
   });
 });
 
 // send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
-    message: 'Route Not Found',
+    message: "Route Not Found",
   });
 });
 
@@ -51,22 +56,21 @@ app.use((err, req, res, next) => {
 });
 
 // set our port
-app.set('port', process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000);
 
 // test the database connection.
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 })();
 
 // Sequelize model synchronization, then start listening on our port.
-sequelize.sync()
-  .then( () => {
-    const server = app.listen(app.get('port'), () => {
-      console.log(`Express server is listening on port ${server.address().port}`);
-    });
+sequelize.sync().then(() => {
+  const server = app.listen(app.get("port"), () => {
+    console.log(`Express server is listening on port ${server.address().port}`);
+  });
 });
